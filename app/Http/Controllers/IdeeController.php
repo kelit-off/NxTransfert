75dc20ee5ab665b1a2f85a2dfdb21fb37d6473b5
Théo Killian <theo.killian5700@gmail.com>
2025-08-04 15:17:42 +0200

    Back opérationnel

--- app/Http/Controllers/IdeeController.php
+++ app/Http/Controllers/IdeeController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Idee;
 use Illuminate\Http\Request;
 
 class IdeeController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $request->validate([
+            'contenue' => 'required|string|max:2000',
+        ]);
+
+        $idee = Idee::create([
+            'contenue' => $request->contenue
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Idee enregistrée avec succès',
+            'idee' =>$idee
+        ]);
+    }
 }
