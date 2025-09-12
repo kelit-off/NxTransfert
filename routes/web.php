133889a62cfe2729567719488bf386dc3284f341
Théo Killian <theo.killian5700@gmail.com>
2025-09-12 09:47:11 +0200

    Test prod

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DownloadController;
 use App\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 Route::get('/', [UploadController::class, "UploadPage"])->name('home');
@@ -18,4 +19,21 @@ Route::get('/dd/{token}', [DownloadController::class, 'downloadZip']);
 
 Route::prefix('admin')->group(function() {
     require __DIR__ . '/admin.php';
+});
+
+Route::get('/test-s3', function() {
+    try {
+        // Test simple
+        $result = Storage::disk('s3')->put('test.txt', 'Hello World');
+        
+        if ($result) {
+            $exists = Storage::disk('s3')->exists('test.txt');
+            return "Upload: OK, Exists: " . ($exists ? 'OK' : 'FAIL');
+        } else {
+            return "Upload: FAIL";
+        }
+        
+    } catch (\Exception $e) {
+        return "Error: " . $e->getMessage();
+    }
 });
